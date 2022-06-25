142a7faaf714b58aaeeef5abb862bead764db734
Jun Heng <zudoturiku@duck.com>
2022-06-25 10:15:11 +0800

    Initial Commit

---
+++ Todo list/Todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_listApp.swift
+//  Todo list
+//
+//  Created by Wong Jun heng on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
